beaf4702bd11caba8decb7988de994a9d27b6687
Karol Loskot <karloskot23@gmail.com>
2025-02-18 18:51:39 +0000

    Implemented player movement

--- Gamejam_Project/Assets/Game_Logic/Player_Control.cs
+++ Gamejam_Project/Assets/Game_Logic/Player_Control.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Player_Control : MonoBehaviour
 {
+    public float sprintSpeed = 8f;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Cursor.lockState = CursorLockMode.Confined;
     }
 
     void Update()
     {
-        
+        //Get player input
+        movement.x = Input.GetAxisRaw("Horizontal"); //For A and D
+        //movement.y = Input.GetAxisRaw("Vertical"); //For W and S (implement maybe for when used with ladders)
+    }
+
+    private void FixedUpdate()
+    {
+        float currentSpeed = moveSpeed;
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            currentSpeed = sprintSpeed;
+        }
+
+        //Translate input to player movement
+        rb.velocity = movement * currentSpeed;
     }
 }
