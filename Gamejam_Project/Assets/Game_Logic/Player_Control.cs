95ecb30b06a3b22ded1a0e21150127e86e3b62f8
Karol Loskot <karloskot23@gmail.com>
2025-02-19 13:24:32 +0000

    Implemented jumping mechanic

--- Gamejam_Project/Assets/Game_Logic/Player_Control.cs
+++ Gamejam_Project/Assets/Game_Logic/Player_Control.cs
@@ -6,20 +6,33 @@ public class Player_Control : MonoBehaviour
 {
     public float sprintSpeed = 8f;
     public float moveSpeed = 5f;
+    public float jumpForce = 14f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
     private Rigidbody2D rb;
     private Vector2 movement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
         //Get player input
         movement.x = Input.GetAxisRaw("Horizontal"); //For A and D
-        movement.y = Input.GetAxisRaw("Vertical"); //For W and S (implement maybe for when used with ladders)
+        //movement.y = Input.GetAxisRaw("Vertical"); //For W and S (implement maybe for when used with ladders)
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     private void FixedUpdate()
@@ -32,6 +45,6 @@ public class Player_Control : MonoBehaviour
         }
 
         //Translate input to player movement
-        rb.velocity = movement * currentSpeed;
+        rb.velocity = new Vector2(movement.x * currentSpeed, rb.velocity.y);
     }
 }
